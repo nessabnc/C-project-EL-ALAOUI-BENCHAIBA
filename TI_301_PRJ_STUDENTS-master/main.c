4baba9da0bd3668f9b21c4b58886e6765ecb93a2
nessabnc <wenessa.benchaiba@efrei.net>
2025-10-21 16:38:03 +0200

    -

--- TI_301_PRJ_STUDENTS-master/main.c
+++ TI_301_PRJ_STUDENTS-master/main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+int maint(){
+  typedef struct s_cell{
+    int value;
+    struct s_cell *next;
+  }t_cell;
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+  typedef struct s_list{
+    t_cell *first;
+    struct s_list *next;
+  }t_list;
+
+ t_cell *createCell(int value){
+   t_cell *newCell = (t_cell *)malloc(sizeof(t_cell));
+   newCell->value = value;
+   newCell->next = NULL;
+   return newCell;
+ }
+
+ t_list *createList(){
+   t_list *newList = (t_list *)malloc(sizeof(t_list));
+   newList->head = NULL;
+   newList->tail = NULL;
+   return newList;
+ }
+
+void add_cell(t_list *list, t_cell *cell){
+  t_cell *newCell = createCell(cell->value);
+  if(list->head == NULL){
+    list->head = newCell;
+    list->tail = newCell;
+    return;
+  }
 }
+
+ void displayList(t_list *head){
+   t_list *current = head;
+   while(current != NULL){
+     printf("%d ", current->value);
+     current = current->next
+   }
+ }
+
